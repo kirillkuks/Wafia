93a370c4328abde0ce22d46d0f30d204c3ced456
Илья Бочкарев <bochkarev.ia@edu.spbstu.ru>
2023-05-09 18:18:02 +0300

    add database

---
+++ src/core/Wafia/Database/Database/NpgsqlConnector.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace WAFIA.Database {
+    public class NpgsqlConnector {
+        public NpgsqlCommand Cmd { get; }
+        public NpgsqlConnector(string host, string username, string password, string database) {
+            string connString = $"Host={host};Username={username};Password={password};Database={database}";
+            NpgsqlConnection nc = new(connString);
+            try {
+                nc.Open();
+            }
+            catch {
+                throw;
+            }
+            Cmd = new() {
+                Connection = nc
+            };
+        }
+        ~NpgsqlConnector() {
+            Cmd.Dispose();
+        }
+    }
+}
