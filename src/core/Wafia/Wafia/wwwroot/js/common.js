b67c5d0a6a4d8f6a6e2922579e8b9d4157e5bb86
Kuksenko <60854701+kirillkuks@users.noreply.github.com>
2023-05-14 22:44:39 +0300

    begin personal area page

--- src/core/Wafia/Wafia/wwwroot/js/common.js
+++ src/core/Wafia/Wafia/wwwroot/js/common.js
@@ -14,4 +14,9 @@ export const EHtmlPages = {
     kGuestScreen: "../GuestScreen.html",
     kPersonalArea: "../PersonalArea1.html",
     kAbout: "../About.html"
-}
+};
+
+export const ClientConsts = {
+    kMaxLoginLength: 50,
+    kMaxPasswordLength: 20
+};
--- src/core/Wafia/Wafia/wwwroot/js/personalArea.js
+++ src/core/Wafia/Wafia/wwwroot/js/personalArea.js
@@ -9,20 +9,40 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import LogInWindowPopUpCreaterComponent from "./logInPopUp.js";
 
 
+
 class PersonalArea extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            userRight: EUserRight.kGuest,
+            userLogin: ""
+        };
     }
 
     render() {
+        (async () => {
+            const responce = await fetch("/api/get_session_info", {
+                method: "POST",
+                headers: { "Accept": "application/json", "Content-Type": "application/json" }
+            })
+
+            const sessionInfo = await responce.json();
+            console.log("user status " + sessionInfo.user_rights);
+
+            if (this.state.userRight != sessionInfo.user_rights)
+            {
+                console.log(sessionInfo.user_login);
+                this.setState({userRight: sessionInfo.user_rights, userLogin: sessionInfo.user_login});
+            }
+        })();
+
         return(
         <div>
-            {this.renderHeader()}
+            { this.renderHeader() }
+            { this.renderMain() }
         </div>
         );
     }
-
 
     renderHeader() {
         return (
@@ -34,11 +54,59 @@ class PersonalArea extends React.Component {
                 <a href={EHtmlPages.kGuestScreen}>
                     <img src="../img/backToGuestScreen.png"></img>
                 </a>
+
+                <h1 style={styles.UserLoginTextStyle}>
+                    {this.state.userLogin}
+                </h1>
             </header>
         );
     }
-}
 
+    renderMain() {
+        return (
+            <main>
+                <img
+                    src="../img/personalAreaBorders.png"
+                    style={styles.PesonalAreaBordersStyle}>
+                </img>
+                {this.renderAdmin()}
+                {this.renderHistoryField()}
+            </main>
+        );
+    }
+
+    renderAdmin() {
+        if (this.state.userRight != EUserRight.kAdmin) {
+            return null;
+        }
+
+        return ([
+            <button
+                key="UpdateDatabaseButton"
+                style={styles.UpdateDatabaseButton}>
+                <p style={styles.ButtonTextStyle}>Update Database</p>
+            </button>,
+            <button
+                key="GiveRightsButton"
+                style={styles.GiveRightsButton}>
+                <p style={styles.ButtonTextStyle}>Give Rights</p>
+            </button>
+        ]);
+    }
+
+    renderHistoryField() {
+        return (
+            <div style={styles.HistoryFieldStyle}>
+                <img src="../img/historyField.png"></img>
+                <button
+                    style={styles.DeleteFromHistoryButtonStyle}
+                    onClick={() => console.log("Delete from history")}>
+                    <img src="../img/deleteFromHistory.png"></img>
+                </button>
+            </div>
+        );
+    }
+}
 
 
 const domContainer = document.getElementById("Main");
